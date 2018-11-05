7a057ee656b63f8289b1b33445db2836b435d066
Unknown <sophiane.benhamed@edu.itescia.fr>
2018-11-05 14:47:10 +0100

    add user and category

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -1,1 +1,20 @@
-/* Create table bankAccount */
+/* Create table user */
+CREATE TABLE user (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    IdUser INT UNSIGNED NOT NULL ,
+    passoword VARCHAR(20)NOT NULL,
+    PRIMARY KEY (id)
+    )
+    CHARACTER SET "utf8"
+    ENGINE = INNODB
+
+/* Create table category */
+CREATE TABLE category (
+    id UNSIGNED NOT NULL AUTO_INCREMENT,
+    idUser INT UNSIGNED NOT NULL,
+    nameOfCategory VARCHAR(15),
+    typeOperation ENUM("debit", "credit")
+    PRIMARY KEY (id)
+)
+CHARACTER SET "utf8"
+ENGINE= INNOB
