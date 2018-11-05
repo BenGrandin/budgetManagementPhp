d9b9ff8a83038dfab01c6fb3b6223415002d2d56
Ben Grandin <bengrandin@hotmail.com>
2018-11-05 14:52:21 +0100

    add table bankAccount

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -1,20 +1,13 @@
-/* Create table user */
-CREATE TABLE user (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    IdUser INT UNSIGNED NOT NULL ,
-    passoword VARCHAR(20)NOT NULL,
-    PRIMARY KEY (id)
-    )
-    CHARACTER SET "utf8"
-    ENGINE = INNODB
+/* Create table bankAccount */
 
-/* Create table category */
-CREATE TABLE category (
-    id UNSIGNED NOT NULL AUTO_INCREMENT,
-    idUser INT UNSIGNED NOT NULL,
-    nameOfCategory VARCHAR(15),
-    typeOperation ENUM("debit", "credit")
-    PRIMARY KEY (id)
-)
-CHARACTER SET "utf8"
-ENGINE= INNOB
+CREATE TABLE bankAccount(
+	id INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT ,
+	userId INT UNSIGNED NOT NULL,
+	accountName VARCHAR (40) NOT NULL,
+	accountType ENUM("checking", "savings", "joint") NOT NULL DEFAULT("checking"),
+	balance INT,
+	currency ENUM("USD", "EUR") NOT NULL DEFAULT("EUR"),
+
+	PRIMARY KEY (id),
+	FOREIGN KEY(userId) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE
+	)
